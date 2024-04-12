4366d8dcd030f48e9e21c21987f9ad717fbae8af
Irinaaissa <irinaalisa7506@gmail.com>
2024-04-12 21:30:36 +0300

    1

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,22 +1,19 @@
-import { Link,useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import css from "./MovieList.module.css";
 
-
-
-export default function MovieList({ movies }) { 
-  const location = useLocation(); 
+export default function MovieList({ movies }) {
+  const location = useLocation();
   console.log(location);
-  return ( 
-      <ul className={css.items}> 
-          { movies && movies.map((movie) => ( 
-              <li 
-              className={css.item}
-               key={ movie.id}> 
-                  <Link to={`/movies/${movie.id}`}
-                      state= {location}  >
-                      {movie.title}</Link> 
-              </li> 
-          ))} 
-      </ul> 
-  ); 
-} 
+  return (
+    <ul className={css.items}>
+      {movies &&
+        movies.map((movie) => (
+          <li className={css.item} key={movie.id}>
+            <Link to={`/movies/${movie.id}`} state={location}>
+              {movie.title}
+            </Link>
+          </li>
+        ))}
+    </ul>
+  );
+}
